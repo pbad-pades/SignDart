9fdfec44f36e998fc8c1c464835df8707989a0a5
Lucas Penido <lpenidoa@me.com>
2021-08-26 15:44:30 -0300

    #9 - Adding more tests to Ed521 algorithm.

--- test/ed521_test.dart
+++ test/ed521_test.dart
@@ -1,34 +1,54 @@
 import 'package:dartffiedlibdecaf/dartffiedlibdecaf.dart';
+import 'package:dartffiedlibdecaf/src/curves/curve.dart';
 import 'package:test/test.dart';
 
 import 'test_vector.dart';
 
 main() {
+  Curve ed521 = TwistedEdwardCurve.ed521();
+
   group('Ed521', () {
-
-    test('Test public key generation', () {
-      var ed521 = TwistedEdwardCurve.ed521();
-
+    test('Test Ed521', () {
       for (var i = 0; i < edd521PrivateKeyVector.length; i++) {
         var privateKey = EdPrivateKey.fromBytes(edd521PrivateKeyVector[i], ed521);
+        var publicKey = privateKey.getPublicKey();
         var publicKeyToCheck = edd521PublicKeyVector[i];
-        expect(publicKeyToCheck, privateKey.getPublicKey().publicKey);
-      }
-    });
 
-    test('Test signing with Ed521', () {
-      var ed521 = TwistedEdwardCurve.ed521();
+        expect(publicKeyToCheck, publicKey.publicKey);
 
-      for (var i = 0; i < edd521SignatureVector.length; i++) {
         var message = messageVector[i];
-        var privateKey = EdPrivateKey.fromBytes(edd521PrivateKeyVector[0], ed521);
+        var signature = privateKey.sign(message);
         var signatureToCheck = edd521SignatureVector[i];
 
-        var signature = privateKey.sign(message);
-
         expect(signatureToCheck, signature);
+
+        var isSignatureValid = publicKey.verify(message, signature);
+
+        expect(isSignatureValid, true);
       }
     });
 
+    test('Test random private keys', () {
+      int keySize = 66;
+      int signatureSize = keySize * 2;
+
+      for (var i = 0; i < 100; i++) {
+        for (var message in messageVector) {
+          var privateKey = EdPrivateKey.generate(ed521);
+          var publicKey = privateKey.getPublicKey();
+
+          expect(privateKey.privateKey.length, keySize);
+          expect(publicKey.publicKey.length, keySize);
+
+          var signature = privateKey.sign(message);
+
+          expect(signature.length, signatureSize);
+
+          var isValid = publicKey.verify(message, signature);
+
+          expect(isValid, true);
+        }
+      }
+    });
   });
 }
