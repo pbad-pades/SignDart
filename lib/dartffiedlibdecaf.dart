eb451d6851b4a706fa61c754cac742a40e9176c1
Lucas Penido <lpenidoa@me.com>
2021-08-27 13:55:25 -0300

    #12 - Removing libdecaf and Ed448 that used the library.

--- lib/dartffiedlibdecaf.dart
+++ lib/dartffiedlibdecaf.dart
@@ -1,5 +1,3 @@
-export 'src/ed448/ed448_private_key.dart';
-export 'src/ed448/ed448_public_key.dart';
 export 'src/curves/twisted_edward_curve.dart';
 export 'src/keys/ed_private_key.dart';
 export 'src/keys/ed_public_key.dart';
