4e5b45c6730dfc2baf3d11920d686a69fcda434a
Lucas Penido <lpenidoa@me.com>
2021-09-01 09:14:00 -0300

    #9 - Adding more tests to Ed521 algorithm.

--- test/ed521_test.dart
+++ test/ed521_test.dart
@@ -1,29 +1,63 @@
+import 'dart:typed_data';
+
 import 'package:signdart/signdart.dart';
 import 'package:test/test.dart';
 
 import 'test_vector.dart';
 
 main() {
-  Curve ed521 = TwistedEdwardCurve.ed521();
-
   group('Ed521', () {
-    test('Test Ed521', () {
+    late Curve ed521;
+
+    setUp(() {
+      ed521 = TwistedEdwardCurve.ed521();
+    });
+
+    group('Test Ed521', () {
+      late Uint8List message;
+      late Uint8List privateKeyBytes;
+      late Uint8List publicKeyBytes;
+      late Uint8List signatureBytes;
+
+      late EdPrivateKey privateKey;
+      late EdPublicKey publicKey;
+      late Uint8List signature;
+
       for (var i = 0; i < ed521PrivateKeyVector.length; i++) {
-        var privateKey = EdPrivateKey.fromBytes(ed521PrivateKeyVector[i], ed521);
-        var publicKey = privateKey.getPublicKey();
-        var publicKeyToCheck = ed521PublicKeyVector[i];
+        setUp(() {
+          message = messageVector[i];
+          publicKeyBytes = ed521PublicKeyVector[i];
+          privateKeyBytes = ed521PrivateKeyVector[i];
+          signatureBytes = ed521SignatureVector[i];
+        });
 
-        expect(publicKeyToCheck, publicKey.publicKey);
+        group('Test $i', () {
+          test('Test public and private keys', () {
+            privateKey = EdPrivateKey.fromBytes(privateKeyBytes, ed521);
+            publicKey = privateKey.getPublicKey();
+            expect(publicKeyBytes, publicKey.publicKey);
+          });
 
-        var message = messageVector[i];
-        var signature = privateKey.sign(message);
-        var signatureToCheck = ed521SignatureVector[i];
+          test('Test sign', () {
+            signature = privateKey.sign(message);
+            expect(signatureBytes, signature);
+          });
 
-        expect(signatureToCheck, signature);
+          test('Test verification', () {
+            bool isSignatureValid = publicKey.verify(message, signature);
+            expect(isSignatureValid, true);
+          });
 
-        var isSignatureValid = publicKey.verify(message, signature);
-
-        expect(isSignatureValid, true);
+          test('Verifying fails when using different public key', () {
+              EdPublicKey differentPublicKey = EdPublicKey.fromBytes(
+                  ed521PublicKeyVector[
+                      (i + 1) % (ed521PublicKeyVector.length - 1)],
+                  ed521);
+              bool isSignatureValid =
+                  differentPublicKey.verify(message, signature);
+              expect(isSignatureValid, false);
+          });
+        });
       }
     });
 
@@ -32,20 +66,34 @@ main() {
       int signatureSize = keySize * 2;
 
       for (var i = 0; i < 100; i++) {
-        for (var message in messageVector) {
-          var privateKey = EdPrivateKey.generate(ed521);
-          var publicKey = privateKey.getPublicKey();
+        for (var j = 0; j < messageVector.length; j++) {
+          Uint8List message = messageVector[j];
+          EdPrivateKey privateKey = EdPrivateKey.generate(ed521);
+          EdPublicKey publicKey = privateKey.getPublicKey();
 
           expect(privateKey.privateKey.length, keySize);
           expect(publicKey.publicKey.length, keySize);
 
-          var signature = privateKey.sign(message);
+          Uint8List signature = privateKey.sign(message);
 
           expect(signature.length, signatureSize);
 
-          var isValid = publicKey.verify(message, signature);
+          bool isValid = publicKey.verify(message, signature);
 
           expect(isValid, true);
+
+          Uint8List differentMessage =
+              messageVector[(j + 1) % (messageVector.length - 1)];
+
+          isValid = publicKey.verify(differentMessage, signature);
+
+          expect(isValid, false);
+
+          Uint8List differentSignature = privateKey.sign(differentMessage);
+
+          isValid = publicKey.verify(message, differentSignature);
+
+          expect(isValid, false);
         }
       }
     });
